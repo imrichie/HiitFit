267cc56a99fcb1f30c004ae725d980ad73e271ee
Richie Flores <rfloresc@icloud.com>
2023-10-30 19:07:18 -0700

    Add new HistoryView to project

---
+++ HiitFit/Views/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  HiitFit
+//
+//  Created by Richie Flores on 10/30/23.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
