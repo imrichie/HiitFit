40e1c3f51ae93c276f26166c942102d5a654839a
Richie Flores <rfloresc@icloud.com>
2023-11-01 13:19:29 -0700

    Add a button view to the HistoryView and add comments

--- HiitFit/Views/HistoryView.swift
+++ HiitFit/Views/HistoryView.swift
@@ -9,19 +9,33 @@ import SwiftUI
 
 struct HistoryView: View {
     
+    // Create an instance of the HistoryStore to fetch and store exercise data
     let history: HistoryStore = HistoryStore()
     
     var body: some View {
-        VStack {
-            Text("History")
-                .font(.title)
-                .padding()
+        ZStack(alignment: .topTrailing) {
             
-            Form {
-                ForEach(history.exerciseDays) { day in
-                    Section(header: Text(day.date.formatted(as: "MMM d")).font(.headline)) {
-                        ForEach(day.exercises, id: \.self) { exercise in
+            Button(action: {}, label: {
+                Image(systemName: "xmark.circle")
+            })
+            .font(.title)
+            .padding(.trailing)
+            
+            VStack {
+                Text("History")
+                    .font(.title)
+                    .padding()
+                
+                Form {
+                    // iterate over each day in the history
+                    ForEach(history.exerciseDays) { day in
+                        // create a section for each day with the day
+                        Section(header: Text(day.date.formatted(as: "MMM d")).font(.headline)) {
+                            // iterate over each exercise for that specific day
+                            ForEach(day.exercises, id: \.self) { exercise in
+                                // display the name of the exercise
                                 Text(exercise)
+                            }
                         }
                     }
                 }
