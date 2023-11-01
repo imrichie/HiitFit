88ce97fbba7d660476ece838a53c6e301c46eb9a
Richie Flores <rfloresc@icloud.com>
2023-11-01 11:23:28 -0700

    Move HistoryStore Extension to new File HistoryStoreDevData

---
+++ HiitFit/Model/HistoryStoreDevData.swift
@@ -0,0 +1,33 @@
+//
+//  HistoryStoreDevData.swift
+//  HiitFit
+//
+//  Created by Richie Flores on 11/1/23.
+//
+
+import Foundation
+
+extension HistoryStore {
+    // dummy data
+    
+    mutating func createDevData() {
+        let yesterday: Date = Date().addingTimeInterval(-86400)
+        let twoDaysAgo: Date = Date().addingTimeInterval(-86400 * 2)
+        
+        let yesterdaysExercises: [String] = [
+            Exercise.exercises[0].exerciseName,
+            Exercise.exercises[1].exerciseName,
+            Exercise.exercises[2].exerciseName
+        ]
+        
+        let twoDaysAgoExercises: [String] = [
+            Exercise.exercises[1].exerciseName,
+            Exercise.exercises[0].exerciseName
+        ]
+        
+        exerciseDays = [
+            ExerciseDay(date: yesterday, exercises: yesterdaysExercises),
+            ExerciseDay(date: twoDaysAgo, exercises: twoDaysAgoExercises)
+        ]
+    }
+}
